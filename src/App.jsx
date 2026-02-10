89b3896184f575db2f362007702e07fb7f6bc63d
nithinWichita <nithinwichita@gmail.com>
2026-02-09 21:35:59 -0600

    chore: initialize React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-6xl font-extrabold text-pink-500">
+        TAILWIND TEST
+      </h1>
+    </div>
+  );
+}
+
